2015e40893af996eea9cdb51b5aee03fc7005b51
Jefferson <jefferson@araguaiasistemas.com.br>
2023-12-11 16:26:58 -0300

    Usando o comando npx create-react-app nomeprojeto

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -2,22 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+    const name = 'Jefferson'
+    const newname = name.toUpperCase() 
+
+     // Jeito correto de fazer gerando funcões
+    function upercase(name){
+      return name.toUpperCase()
+    }
+
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+/*
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+*/
+          
+
         >
-          Learn React
+          <p>{name} </p>
+          <p>{newname} </p>
+
+          Primeiro Projeto React
+          <p>{upercase('jefferson barbosa lima')} </p>
+           
         </a>
       </header>
+
+
     </div>
   );
 }
